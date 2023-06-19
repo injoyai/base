54db596670d3941ea962859639282e4bcf568876
injoy <1113655791@qq.com>
2023-06-19 16:24:14 +0800

    小优化

--- g/g_const.go
+++ g/g_const.go
@@ -1,12 +1,19 @@
 package g
 
-import "errors"
+import (
+	"errors"
+	"time"
+)
 
 const (
 	Null        = ""
 	TimeDefault = "2006-01-02 15:04:05"
 	TimeDate    = "2006-01-02"
 	TimeTime    = "15:04:05"
+)
+
+var (
+	StartTime = time.Now()
 )
 
 var (
