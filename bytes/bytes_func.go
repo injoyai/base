775f27ea20da7eb198e82efdfd6dc87575a2941e
injoyai <1113655791@qq.com>
2024-01-24 13:39:55 +0800

    优化bytes.Equal

--- bytes/bytes_func.go
+++ bytes/bytes_func.go
@@ -21,15 +21,7 @@ func Equal(bs1, bs2 []byte) bool {
 	if (bs1 == nil) != (bs2 == nil) {
 		return false
 	}
-	if len(bs1) != len(bs2) {
-		return false
-	}
-	for i := 0; i < len(bs1); i++ {
-		if bs1[i] != bs2[i] {
-			return false
-		}
-	}
-	return true
+	return bytes.Equal(bs1, bs2)
 }
 
 // Sum 字节校验和
