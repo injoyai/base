7cd08d3d4449a7d72784fc2bc19d978b8aa672cd
injoyai <1113655791@qq.com>
2024-05-24 14:29:59 +0800

    优化timeout.Timeout

--- maps/timeout/map_timeout.go
+++ maps/timeout/map_timeout.go
@@ -7,32 +7,23 @@ import (
 )
 
 func New() *Timeout {
-	return NewWithContext(context.Background())
-}
-
-func NewWithContext(ctx context.Context) *Timeout {
-	ctx, cancel := context.WithCancel(ctx)
 	return &Timeout{
 		interval:    time.Second * 10,
 		timeout:     time.Minute,
 		timeoutFunc: nil,
 		m:           maps.NewSafe(),
-		ctx:         ctx,
-		cancel:      cancel,
 	}
 }
 
 // Timeout 超时机制
 type Timeout struct {
-	interval    time.Duration //检查间隔
-	timeout     time.Duration //超时时间
-	timeoutFunc func()        //超时执行的函数
-	m           *maps.Safe    //
-	ctx         context.Context
-	cancel      context.CancelFunc
+	interval    time.Duration               //检查间隔
+	timeout     time.Duration               //超时时间
+	timeoutFunc func(key interface{}) error //超时执行的函数
+	m           *maps.Safe                  //
 }
 
-func (this *Timeout) SetDealFunc(fn func()) *Timeout {
+func (this *Timeout) SetDealFunc(fn func(key interface{}) error) *Timeout {
 	this.timeoutFunc = fn
 	return this
 }
@@ -55,22 +46,22 @@ func (this *Timeout) Del(key interface{}) {
 	this.m.Del(key)
 }
 
-func (this *Timeout) Run() {
+func (this *Timeout) Run(ctx context.Context) {
 	for {
 		select {
-		case <-this.ctx.Done():
+		case <-ctx.Done():
 			return
 		case <-time.After(this.interval):
+			now := time.Now()
 			this.m.Range(func(key, value interface{}) bool {
-				if time.Now().Sub(value.(time.Time)) > this.timeout {
-					if this.timeoutFunc != nil {
-						this.timeoutFunc()
+				if now.Sub(value.(time.Time)) > this.timeout {
+					if this.timeoutFunc == nil && this.timeoutFunc(key) == nil {
+						//超时函数未设置,或者执行成功,则删除缓存
 						this.m.Del(key)
 					}
 				}
 				return true
 			})
 		}
-
 	}
 }
