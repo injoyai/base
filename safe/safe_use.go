b19422ed163c5092d8270cb6853af77d67cf6d87
injoyai <1113655791@qq.com>
2024-08-23 14:33:00 +0800

    增加占用对象,safe.Use

--- safe/safe_use.go
+++ safe/safe_use.go
@@ -2,20 +2,13 @@ package safe
 
 import "sync/atomic"
 
-func NewUse(f ...func()) *Use {
-	return &Use{
-		OnUser: func() {
-			for _, v := range f {
-				v()
-			}
-		},
-	}
-}
+func NewUse() *Use { return &Use{} }
 
 type Use struct {
 	used uint32
 	//成功占用执行的事件,例初始化操作
-	OnUser func()
+	OnUse   func()
+	OnUnUse func()
 }
 
 // Use 尝试占用,返回是否占用成功,失败标识被其他进程占用
@@ -23,15 +16,21 @@ func (this *Use) Use() bool {
 	if !atomic.CompareAndSwapUint32(&this.used, 0, 1) {
 		return false
 	}
-	if this.OnUser != nil {
-		this.OnUser()
+	if this.OnUse != nil {
+		this.OnUse()
 	}
 	return true
 }
 
 // UnUse 取消占用
 func (this *Use) UnUse() bool {
-	return atomic.CompareAndSwapUint32(&this.used, 1, 0)
+	if !atomic.CompareAndSwapUint32(&this.used, 1, 0) {
+		return false
+	}
+	if this.OnUnUse != nil {
+		this.OnUnUse()
+	}
+	return true
 }
 
 // Used 是否被占用
