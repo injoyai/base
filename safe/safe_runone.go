a014925c4818c427b18337e324f62051d66b0bc3
injoyai <1113655791@qq.com>
2024-09-24 10:12:49 +0800

    增加safe.RunOne,有且只有1个在运行,新运行的需要等待老的运行完成

---
+++ safe/safe_runone.go
@@ -0,0 +1,77 @@
+package safe
+
+import (
+	"context"
+	"errors"
+	"sync"
+	"sync/atomic"
+)
+
+/*
+RunOne
+最多只有一个在运行,是否需要等待上一个结束?
+*/
+type RunOne interface {
+	Run() error
+	Running() bool
+	Close() error
+}
+
+func NewRunOne(fn func(ctx context.Context) error) RunOne {
+	return &runOne{
+		fn:   fn,
+		pool: sync.Pool{New: func() interface{} { return make(chan struct{}) }},
+	}
+}
+
+type runOne struct {
+	cancel  context.CancelFunc
+	running uint32
+	mu      sync.Mutex
+	fn      func(ctx context.Context) error
+	pool    sync.Pool
+	done    chan struct{}
+}
+
+func (this *runOne) Run() (err error) {
+
+	if this.fn == nil {
+		return errors.New("未设置函数")
+	}
+
+	this.mu.Lock()
+	if this.cancel != nil {
+		this.cancel()
+	}
+	if this.done != nil {
+		//等待上次结束
+		<-this.done
+	}
+	this.done = this.pool.Get().(chan struct{})
+	ctx, cancel := context.WithCancel(context.Background())
+	this.cancel = cancel
+	this.mu.Unlock()
+
+	defer func() {
+		atomic.StoreUint32(&this.running, 0)
+		close(this.done)
+	}()
+
+	atomic.StoreUint32(&this.running, 1)
+	return this.fn(ctx)
+
+}
+
+func (this *runOne) Running() bool {
+	return atomic.LoadUint32(&this.running) == 1
+}
+
+func (this *runOne) Close() error {
+	this.mu.Lock()
+	defer this.mu.Unlock()
+	if this.cancel != nil {
+		this.cancel()
+		this.cancel = nil
+	}
+	return nil
+}
---
+++ safe/safe_runone_test.go
@@ -0,0 +1,29 @@
+package safe
+
+import (
+	"context"
+	"sync/atomic"
+	"testing"
+	"time"
+)
+
+func TestNewRunOne(t *testing.T) {
+	n := uint32(0)
+	x := NewRunOne(func(ctx context.Context) error {
+		atomic.AddUint32(&n, 1)
+		for {
+			select {
+			case <-ctx.Done():
+				t.Log("close", n)
+				return nil
+			case <-time.After(time.Second):
+				t.Log("run", n)
+			}
+		}
+	})
+	for i := 0; i < 10; i++ {
+		<-time.After(time.Millisecond * 100)
+		go x.Run()
+	}
+	time.Sleep(time.Second * 10)
+}
