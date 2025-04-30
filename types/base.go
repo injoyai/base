e71053245eee582f86edb7e6ebf3ca3160283261
injoyai <1113655791@qq.com>
2025-04-30 10:24:52 +0800

    优化

--- types/base.go
+++ types/base.go
@@ -8,7 +8,7 @@ import (
 
 type Integer interface {
 	~int | ~int8 | ~int16 | ~int32 | ~int64 |
-		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
 }
 
 type Floater interface {
@@ -20,7 +20,7 @@ type Number interface {
 	Floater
 }
 
-type Ctx interface {
+type Signaler interface {
 	Done() <-chan struct{}
 	Err() error
 }
@@ -30,10 +30,18 @@ type Err string
 
 func (this Err) Error() string { return string(this) }
 
+func (this Err) Is(target error) bool {
+	return this.Error() == target.Error()
+}
+
 type Debugger bool
 
 func (this *Debugger) Debug(b ...bool) {
 	*this = Debugger(len(b) == 0 || b[0])
+}
+
+func (this *Debugger) Debugged() bool {
+	return bool(*this)
 }
 
 type Sorter interface {
@@ -52,14 +60,27 @@ type Runner interface {
 }
 
 type Cacher[K comparable, V any] interface {
+	// Get retrieves a value by key. Returns error if not found or expired.
 	Get(key K) (V, error)
+
+	// Set stores a value with an optional expiration.
 	Set(key K, value V, expiration ...time.Duration) error
+
+	// Del deletes a value by key.
 	Del(key K) error
 }
 
 type Mapper[K comparable, V any] interface {
-	Get(K) (V, bool)
-	Set(K, V)
-	Del(K)
+	// Get retrieves a value for a key. Returns false if the key is not present.
+	Get(key K) (V, bool)
+
+	// Set stores or overwrites the value for a key.
+	Set(key K, value V)
+
+	// Del deletes the value associated with a key.
+	Del(key K)
+
+	// Range iterates over all key-value pairs.
+	// If the function returns false, iteration stops.
 	Range(func(K, V) bool)
 }
