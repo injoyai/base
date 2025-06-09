389ce9e20712c212639a29a95688e82ae43ccec5
injoyai <1113655791@qq.com>
2025-06-09 16:34:52 +0800

    优化WaitLimit和LimitGo

--- chans/chan_limit_go.go
+++ chans/chan_limit_go.go
@@ -1,35 +1,27 @@
 package chans
 
-import (
-	"sync"
-)
-
 type LimitGo interface {
 	Go(f func())
 	Wait()
 }
 
 func NewLimitGo(limit int) LimitGo {
-	return &limitGo{c: make(chan struct{}, limit)}
+	return &limitGo{limit: NewWaitLimit(limit)}
 }
 
 type limitGo struct {
-	wg sync.WaitGroup
-	c  chan struct{}
+	limit WaitLimit
 }
 
 func (this *limitGo) Go(f func()) {
-	this.wg.Add(1)
-	this.c <- struct{}{}
+
+	this.limit.Add()
 	go func() {
-		defer func() {
-			<-this.c
-			this.wg.Done()
-		}()
+		defer this.limit.Done()
 		f()
 	}()
 }
 
 func (this *limitGo) Wait() {
-	this.wg.Wait()
+	this.limit.Wait()
 }
--- chans/chan_wait_limit.go
+++ chans/chan_wait_limit.go
@@ -4,26 +4,32 @@ import (
 	"sync"
 )
 
-// NewWaitLimit sync.WaitGroup基础上加了同时释放的数量
-func NewWaitLimit(limit int) *WaitLimit {
-	return &WaitLimit{c: make(chan struct{}, limit)}
+type WaitLimit interface {
+	Add()
+	Done()
+	Wait()
 }
 
-type WaitLimit struct {
+// NewWaitLimit sync.WaitGroup基础上加了同时释放的数量
+func NewWaitLimit(limit int) WaitLimit {
+	return &waitLimit{c: make(chan struct{}, limit)}
+}
+
+type waitLimit struct {
 	wg sync.WaitGroup
 	c  chan struct{}
 }
 
-func (this *WaitLimit) Add() {
+func (this *waitLimit) Add() {
 	this.wg.Add(1)
 	this.c <- struct{}{}
 }
 
-func (this *WaitLimit) Done() {
+func (this *waitLimit) Done() {
 	<-this.c
 	this.wg.Done()
 }
 
-func (this *WaitLimit) Wait() {
+func (this *waitLimit) Wait() {
 	this.wg.Wait()
 }
