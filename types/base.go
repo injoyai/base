649dc0df35d43cba7b0925bf4746c789146ffa37
injoyai <1113655791@qq.com>
2025-06-13 10:23:41 +0800

    增加types.Comparable,同1.21版本的cmp.Ordered

--- types/base.go
+++ types/base.go
@@ -24,8 +24,9 @@ type Floater interface {
 }
 
 type Number interface {
-	Integer
-	Floater
+	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr |
+	~float32 | ~float64
 }
 
 type Signaler interface {
