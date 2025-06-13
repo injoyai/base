9fa1f677581da0a6c31b8ca810949db11e4bfaf9
injoyai <1113655791@qq.com>
2025-06-13 10:23:04 +0800

    增加types.Comparable,同1.21版本的cmp.Ordered

--- types/base.go
+++ types/base.go
@@ -6,9 +6,17 @@ import (
 	"time"
 )
 
+// Comparable 可比较,同1.21版本的cmp.Ordered
+type Comparable interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 |
+	~float32 | ~float64 |
+	~string
+}
+
 type Integer interface {
 	~int | ~int8 | ~int16 | ~int32 | ~int64 |
-		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
 }
 
 type Floater interface {
