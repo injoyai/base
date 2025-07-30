ccce6e6308fb5454e10f17517bf8a1f663dda10e
injoyai <1113655791@qq.com>
2025-07-30 13:34:08 +0800

    增加safe.Pool,用来解决sync.Pool的一些问题,如不能被gc导致系统内存无限变大

--- safe/safe_pool.go
+++ safe/safe_pool.go
@@ -1,8 +1,8 @@
 package safe
 
-func NewPool[T any](max int, new func() T) *Pool[T] {
+func NewPool[T any](max int, new func() *T) *Pool[T] {
 	return &Pool[T]{
-		ch:  make(chan T, max),
+		ch:  make(chan *T, max),
 		new: new,
 	}
 }
@@ -10,12 +10,12 @@ func NewPool[T any](max int, new func() T) *Pool[T] {
 // Pool 内存复用池,用来替代sync.Pool
 // 用来解决sync.Pool内存不能被系统回收的问题
 type Pool[T any] struct {
-	ch  chan T
-	new func() T
+	ch  chan *T
+	new func() *T
 }
 
 // Get 获取对象,如果没有会新申请
-func (this *Pool[T]) Get() T {
+func (this *Pool[T]) Get() *T {
 	select {
 	case buf := <-this.ch:
 		return buf
@@ -27,8 +27,11 @@ func (this *Pool[T]) Get() T {
 // Put 回收对象,需要对象的内存地址,方便解除引用让系统回收
 // 每个变量的申明都会有一个固定的内存地址,注意变量,指针,内存地址的区别
 func (this *Pool[T]) Put(buf *T) {
+	if buf == nil {
+		return
+	}
 	select {
-	case this.ch <- *buf:
+	case this.ch <- buf:
 	default:
 		//解除引用,然后让GC回收
 		*buf = *new(T)
